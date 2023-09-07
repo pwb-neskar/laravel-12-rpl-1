7e1be84f7a9256a4abf5a033d46c51d51fce320d
ahmadjumadi <ahmadjumadi@smkn1karawang.sch.id>
2023-09-07 14:13:47 +0700

    add migration

---
+++ database/migrations/2023_09_07_064719_create_perans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('perans', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('film_id');
+            $table->unsignedBigInteger('cast_id');
+            $table->string('nama', 45);
+            $table->foreign('film_id')->references('id')->on('films');
+            $table->foreign('cast_id')->references('id')->on('casts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('perans');
+    }
+};
